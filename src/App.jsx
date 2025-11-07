d98a26afa8f7bf6a4d5de3793cd673c9ae19f73b
Tom Zhang <tomzhang.canada888@gmail.com>
2025-11-06 23:37:15 -0500

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,27 @@
 import { gsap } from "gsap"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-import { useGSAP } from '@gsap/react'
-import './App.css'
+import Hero from './Hero'
 
 gsap.registerPlugin(ScrollTrigger)
 
+// on different devices
+// cloud overflow hidden
+
 function App() {
-  useGSAP(() => {
-    const tl = gsap.timeline();
-    tl.from(".hello-text", {
-      y: "-86vh",
-    }, 0)
-    .from(".mountain-image", {
-      scale: 1.2,
-    }, 0)
-    .to(".cloud1", {
-      y: "-40vh",
-      x: "-100vh"
-    }, 0)
-    .to(".cloud2", {
-      y: "-50vh",
-      x: "-30vh"
-    }, 0)
-    .to(".cloud3", {
-      y: "-50vh",
-      x: "30vh"
-    }, 0)
-    .to(".cloud4", {
-      y: "-40vh",
-      x: "100vh"
-    }, 0)
-
-    .to(".cloud5", {
-      x: "-100vh"
-    }, 0)
-    .to(".cloud6", {
-      y: "30vh",
-      x: "-120vh"
-    }, 0)
-    .to(".cloud7", {
-      y: "30vh",
-      x: "100vh"
-    }, 0)
-    .to(".cloud8", {
-      x: "100vh"
-    }, 0)
-    
-    .to(".cloud9", {
-      y: "30vh",
-    }, 0)
-    .to(".cloud10", {
-      y: "30vh",
-    }, 0)
-    .to(".cloud11", {
-      y: "30vh",
-    }, 0)
-    .to(".cloud12", {
-      y: "30vh",
-    }, 0);
-
-    ScrollTrigger.create({
-      animation: tl,
-      trigger: '#mountain',
-      start: 'top top',
-      end: '+=3000',
-      scrub: 1,
-      pin: true,
-    })
-  });
-
   return (
     <div className='h-[5000px]'>
-      <div className='relative' id='mountain'>
-        {/* background images */}
-        <div className='h-screen w-screen absolute overflow-hidden'>
-          <div className='h-full w-full bg-cover bg-center mountain-image' style={{ backgroundImage: "url('/man_on_mountain.jpeg')" }}></div>
-        </div>
-        <div className='h-screen w-screen absolute z-20 overflow-hidden'>
-          <div className='h-full w-full bg-cover bg-center mountain-image' style={{ backgroundImage: "url('/man.png')" }}></div>
-        </div>
-        
-        {/* hello text */}
-        <h1 className='font-hyhero text-9xl text-red-500 hello-text absolute z-10 top-[26vh] left-[30vh]' style={{ textShadow: '5px -5px 3px white' }}>Hi There</h1>
-        <h1 className='font-hyhero text-9xl text-red-500 hello-text absolute z-10 top-[46vh] left-[67vh]' style={{ textShadow: '5px -5px 3px white' }}>This is Tom</h1>
+      <Hero />
 
-        {/* clouds */}
-        <div className='h-screen w-screen overflow-hidden absolute z-30'>
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[-15vh] left-[-20vh] cloud1' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[-15vh] left-[30vh] cloud2' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[-15vh] left-[70vh] cloud3' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[-15vh] left-[120vh] cloud4' />
+      {/* <div className="w-64 h-32 bg-black text-white p-4 overflow-hidden text-ellipsis">
+        This is a long text that will be cut off inside the black box.
+      </div> */}
 
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[20vh] left-[-20vh] cloud5' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[20vh] left-[30vh] cloud6' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[20vh] left-[80vh] cloud7' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[20vh] left-[130vh] cloud8' />
-
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[56vh] left-[-20vh] cloud9' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[56vh] left-[30vh] cloud10' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[56vh] left-[80vh] cloud11' />
-          <img src="/cloud.webp" alt="cloud" className='h-[50vh] max-w-none absolute top-[56vh] left-[130vh] cloud12' />
+      {/* ending */}
+      <div className='relative h-screen'>
+        <div className=''>
+          
         </div>
+
       </div>
   </div>
   )
---
+++ src/Hero.jsx
@@ -0,0 +1,115 @@
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+import { useGSAP } from '@gsap/react'
+import './Hero.css'
+
+function Hero() {
+  const cloudPositions = [
+    { top: "-15vh", left: "-20vh" },
+    { top: "-15vh", left: "30vh" },
+    { top: "-15vh", left: "70vh" },
+    { top: "-15vh", left: "120vh" },
+    { top: "20vh", left: "-20vh" },
+    { top: "20vh", left: "30vh" },
+    { top: "20vh", left: "80vh" },
+    { top: "20vh", left: "130vh" },
+    { top: "56vh", left: "-20vh" },
+    { top: "56vh", left: "30vh" },
+    { top: "56vh", left: "80vh" },
+    { top: "56vh", left: "130vh" },
+  ]
+
+  useGSAP(() => {
+    const heroTL = gsap.timeline();
+    heroTL.from(".hello-text", {
+      y: "-86vh",
+    }, 0)
+    .from(".mountain-image", {
+      scale: 1.2,
+    }, 0)
+    .to("#cloud1", {
+      y: "-40vh",
+      x: "-100vh"
+    }, 0)
+    .to("#cloud2", {
+      y: "-50vh",
+      x: "-30vh"
+    }, 0)
+    .to("#cloud3", {
+      y: "-50vh",
+      x: "30vh"
+    }, 0)
+    .to("#cloud4", {
+      y: "-40vh",
+      x: "100vh"
+    }, 0)
+
+    .to("#cloud5", {
+      x: "-100vh"
+    }, 0)
+    .to("#cloud6", {
+      y: "30vh",
+      x: "-120vh"
+    }, 0)
+    .to("#cloud7", {
+      y: "30vh",
+      x: "100vh"
+    }, 0)
+    .to("#cloud8", {
+      x: "100vh"
+    }, 0)
+    
+    .to("#cloud9", {
+      y: "30vh",
+    }, 0)
+    .to("#cloud10", {
+      y: "30vh",
+    }, 0)
+    .to("#cloud11", {
+      y: "30vh",
+    }, 0)
+    .to("#cloud12", {
+      y: "30vh",
+    }, 0);
+
+    ScrollTrigger.create({
+      animation: heroTL,
+      trigger: '#mountain',
+      start: 'top top',
+      end: '+=3000',
+      scrub: 1,
+      pin: true,
+    })
+  });
+
+  return (
+      <div className='h-screen relative' id='mountain'>
+        {/* background images */}
+        <div className='h-screen w-screen absolute overflow-hidden'>
+          <div className='h-full w-full bg-cover bg-center mountain-image' style={{ backgroundImage: "url('/man_on_mountain.jpeg')" }}></div>
+        </div>
+        <div className='h-screen w-screen absolute z-20 overflow-hidden'>
+          <div className='h-full w-full bg-cover bg-center mountain-image' style={{ backgroundImage: "url('/man.png')" }}></div>
+        </div>
+        
+        {/* hello text */}
+        <h1 className='font-hyhero text-9xl text-red-500 hello-text absolute z-10 top-[26vh] left-[30vh]' style={{ textShadow: '5px -5px 3px white' }}>Hi There</h1>
+        <h1 className='font-hyhero text-9xl text-red-500 hello-text absolute z-10 top-[46vh] left-[67vh]' style={{ textShadow: '5px -5px 3px white' }}>This is Tom</h1>
+
+        {/* clouds */}
+        <div className='h-screen w-screen overflow-hidden absolute z-30'>
+          {cloudPositions.map((position, index) => (
+            <Cloud key={index} id={`cloud${index+1}`} top={position.top} left={position.left} />
+          ))}
+        </div>
+      </div>
+  )
+}
+
+function Cloud({ id, top, left }) {
+  return (
+    <img id={id} src="/cloud.webp" alt="cloud" className={`h-[50vh] max-w-none absolute cloud`} style={{ top, left }} />
+  )
+}
+
+export default Hero
